1423b2ab7f8a8407a55521c2ff7321ccfb656b1b
aidenzzz <yosefaiden@gmail.com>
2023-12-17 17:39:37 -0500

    init

--- desktop/target/debug/deps/chip8_core-f8619007eba76823.d
+++
@@ -1,7 +0,0 @@
-/home/aiden/documents/repos/chip8rust/desktop/target/debug/deps/chip8_core-f8619007eba76823.rmeta: /home/aiden/documents/repos/chip8rust/chip8_core/src/lib.rs
-
-/home/aiden/documents/repos/chip8rust/desktop/target/debug/deps/libchip8_core-f8619007eba76823.rlib: /home/aiden/documents/repos/chip8rust/chip8_core/src/lib.rs
-
-/home/aiden/documents/repos/chip8rust/desktop/target/debug/deps/chip8_core-f8619007eba76823.d: /home/aiden/documents/repos/chip8rust/chip8_core/src/lib.rs
-
-/home/aiden/documents/repos/chip8rust/chip8_core/src/lib.rs:
--- desktop/target/debug/desktop.d
+++
@@ -1,1 +0,0 @@
-/home/aiden/documents/repos/chip8rust/desktop/target/debug/desktop: /home/aiden/documents/repos/chip8rust/chip8_core/src/lib.rs /home/aiden/documents/repos/chip8rust/desktop/src/main.rs
