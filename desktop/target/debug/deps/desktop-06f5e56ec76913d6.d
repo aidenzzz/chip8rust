a82103d2dc1d8690debfd9309769254a25aa7f64
aidenzzz <yosefaiden@gmail.com>
2023-12-17 03:15:44 -0500

    init

---
+++ desktop/target/debug/deps/desktop-06f5e56ec76913d6.d
@@ -0,0 +1,5 @@
+/home/aiden/documents/repos/chip8rust/desktop/target/debug/deps/desktop-06f5e56ec76913d6: src/main.rs
+
+/home/aiden/documents/repos/chip8rust/desktop/target/debug/deps/desktop-06f5e56ec76913d6.d: src/main.rs
+
+src/main.rs:
